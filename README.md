34a9ec862b68ef8c5e7d85d388099b80231aebfd
austin ce <austin.cawley@gmail.com>
2020-06-01 14:00:01 -0400

    simulate backpressure and checkpointing

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # Backpressure-aware Consumer Prototype
 
-For [FLINK-10195](https://issues.apache.org/jira/browse/FLINK-10195?jql=project%20%3D%20FLINK%20AND%20status%20%3D%20Open%20AND%20component%20%3D%20%22Connectors%2F%20RabbitMQ%22)
-
-## Replacing the QueuingConsumer
-
-Buffering the messages seems to still be a good approach, so messages can be pulled one-by-one
-in the source read loop to keep the implementation there simple. We just need a way control
-the flow of messages from the RMQ server.
+For [FLINK-10195], a simple RabbitMQ queued consumer implementation that sets a prefetch to the buffer size
+and simulates checkpointing. Backpressure is simulated by sleeping after pulling a message on a periodic function
+that dips between being faster and slower than the producer.
 
 ## [Current PR (#8111)](https://github.com/apache/flink/pull/8111)
 
@@ -15,45 +11,57 @@ much data is sent to the client/ under backpressure. This has shown to be too co
 for production use, at around  ~60 messages/s.
 
 The queue length mechanism works well and is used in other connectors (ex: kinesis).
+Messages can be pulled one-by-one from the source read loop to keep the implementation there simple.
+We just need a way control the flow of messages from the RMQ server.
 
 ## Proposed Changes
+
+__tldr;__
+Set `prefetch` count (aka `basicQos`) to be the size of a configurable buffer.
+
 
 Instead of starting/ stopping, we can adjust the `prefetch` (see: [1] and [2]) amount on the RMQ channel, which
 is the only way RMQ allows client-side flow control. 
 
-With this prototype, the rough metrics seem to be very performant and keep up with the publishing. When the consumer
-is throttled, the publish rate is set to be close to what can be consumed, and picks ups quickly.
-
+With this prototype, the rough metrics seem to be performant and keep up with the publishing. When the consumer
+is throttled (i.e. the unacked count == prefetch limit), the publish rate is set to be close to what can be consumed,
+and picks ups quickly as soon as there is space in the buffer.
 
 ### Running
 
 ```bash
 $ docker-compose up &
-$ node index.js | grep stats
+$ node index.js --purge | grep stats
 # ...
-stats {"ts":"2020-06-01T00:54:19.327Z","queue":{"unacked":69,"ready":0,"publish/s":854.4,"deliver/s":854.4},"consumer":{"buffer length":62,"prefetch":75},"session":{"count":9},"approxUnacked":{"estimate":71,"delta":-2,"% error":-2.9}}
-stats {"ts":"2020-06-01T00:54:19.831Z","queue":{"unacked":69,"ready":0,"publish/s":854.4,"deliver/s":854.4},"consumer":{"buffer length":61,"prefetch":75},"session":{"count":9},"approxUnacked":{"estimate":70,"delta":-1,"% error":-1.45}}
-stats {"ts":"2020-06-01T00:54:20.334Z","queue":{"unacked":69,"ready":0,"publish/s":854.4,"deliver/s":854.4},"consumer":{"buffer length":63,"prefetch":75},"session":{"count":5},"approxUnacked":{"estimate":68,"delta":1,"% error":1.45}}
+stats {"ts":"2020-06-01T17:37:17.679Z","queue":{"unacked":322,"ready":0,"publish/s":54.4,"deliver/s":99.6,"delta/s":-45.2},"consumer":{"processing dur":19.97,"buffer length":68,"buffer space":432,"prefetch":500,"prefetch space":178},"session":{"session length":432},"unacked":{"estimate":500,"delta":-178,"% error":-55.28}}
+stats {"ts":"2020-06-01T17:37:18.190Z","queue":{"unacked":322,"ready":0,"publish/s":54.4,"deliver/s":99.6,"delta/s":-45.2},"consumer":{"processing dur":16.06,"buffer length":43,"buffer space":457,"prefetch":500,"prefetch space":178},"session":{"session length":457},"unacked":{"estimate":500,"delta":-178,"% error":-55.28}}
+stats {"ts":"2020-06-01T17:37:18.701Z","queue":{"unacked":500,"ready":95,"publish/s":54.6,"deliver/s":0.4,"delta/s":54.2},"consumer":{"processing dur":24.29,"buffer length":11,"buffer space":489,"prefetch":500,"prefetch space":0},"session":{"session length":489},"unacked":{"estimate":500,"delta":0,"% error":0}}
 ```
 
-## Potential Issues
+### Potential Issues
 
 * `global` prefetch
   * The meaning changes between versions of RMQ, but since the current `RMQSource` controls
-    both the Connection and the Channel, we might be ok. The only case this isn't true is when
-    users override one or both. `false` is the usual setting and will likely work for us.
-  *  see: https://www.squaremobius.net/amqp.node/channel_api.html#channel_prefetch  
+    both the Connection and the Channel, we might be ok.
+  * The only case this isn't true is when users override one or both.
+    `false` is the usual setting and will likely work for us.
+  * see: https://www.squaremobius.net/amqp.node/channel_api.html#channel_prefetch  
      _RabbitMQ v3.3.0 changes the meaning of prefetch (basic.qos) to apply per-consumer, rather than per-channel.
      It will apply to consumers started after the method is called. See rabbitmq-prefetch._
+* Limiting-by-channel
+  * A Channel can be used to read from many queues, so setting a `prefetch` has to be synced
+    across each queue on the server, which is slow on clusters.
+  * Like above, since the `RMQSource` currently controls the Channel and only reads from
+    a single queue. Will only be a problem if users override poorly.
 * Dynamically setting the `prefetch`
-  * If the `prefetch` set at the creation needs to change, an example
-    of changing this on the fly can be found here: https://github.com/MassTransit/MassTransit/blob/master/src/MassTransit.RabbitMqTransport.Tests/SetPrefetchCount_Specs.cs   
+  * If the `prefetch` set at the creation needs to change, an example project that
+    changes this on the fly can be found here: https://github.com/MassTransit/MassTransit/blob/master/src/MassTransit.RabbitMqTransport.Tests/SetPrefetchCount_Specs.cs   
 
 ## Tickets addressed
 
 - [FLINK-10195]
 - [FLINK-6885] RMQSource does not support qos, leading to oom
-  - we could allow this prefect mechanism to be configurable
+  - we should allow this prefetch/ max buffer length mechanism to be configurable
 - [FLINK-17559] Duplicate of `FLINK-6885`
 
 [1]: https://www.rabbitmq.com/consumer-prefetch.html
--- index.js
+++ index.js
@@ -13,15 +13,21 @@ const password = 'guest';
 const QUEUE_NAME = 'testQueue';
 
 // Parameters
+const PUBLISH_HEAD_START_MS = 1000;
+const PUBLISH_SLEEP_MS = 18;
 
-const PUBLISH_SLEEP_MS = 0;
-const PUBLISHER_HEAD_START_MS = 100; // let the queue fill up a bit before reading
+const CONSUMER_MAX_BUFFER_LENGTH = 500;
 
-const CONSUMER_MAX_BUFFER_LENGTH = 50;
+//// Periodic backpressuring
+const RUN_LOOP_SLEEP_MS = 15; // base duration for "processing" for each message
+const RUN_LOOP_SINE_COEFF = 10;
+const RUN_LOOP_SINE_ANGLE_COEFF = 0.25;
 
-const RUN_LOOP_SLEEP_MS = 100; // time for "processing" for each message
-const RUN_LOOP_CHECKPOINT_INTERVAL_MS = 5000;
+//// Checkpointing
+const CHECKPOINT_INTERVAL_MS = 10000;
+const CHECKPOINT_POLL_MS = 500;
 
+//// Stats sampling
 const STATS_LOOP_SLEEP_MS = 500;
 
 // State
@@ -38,6 +44,8 @@ let sessionMessages;
 let isRunning = true;
 
 let shouldPurgeQueue = false;
+
+let lastProcessingDuration = 0;
 
 // Utilities
 
@@ -82,19 +90,21 @@ async function sampleStats() {
     queue: {
       unacked: queue.messages_unacknowledged,
       ready: queue.messages_ready,
-      'publish/s': queue.message_stats && queue.message_stats.publish_details.rate,
-      'deliver/s': queue.message_stats && queue.message_stats.deliver_details.rate,
+      'publish/s': !queue.message_stats ? 'N/A' : queue.message_stats.publish_details.rate,
+      'deliver/s': !queue.message_stats ? 'N/A' : queue.message_stats.deliver_details.rate,
+      'delta/s': !queue.message_stats ? 'N/A' : round2(queue.message_stats.publish_details.rate - queue.message_stats.deliver_details.rate),
     },
     consumer: {
+      'processing dur': round2(lastProcessingDuration),
       'buffer length': consumer.buffer.length,
-      remaining: CONSUMER_MAX_BUFFER_LENGTH - consumer.buffer.length,
-      prefetch: consumer.prefetchAmount,
+      'buffer space': CONSUMER_MAX_BUFFER_LENGTH - consumer.buffer.length,
+      prefetch: consumer.prefetchCount,
+      'prefetch space': consumer.prefetchCount - queue.messages_unacknowledged,
     },
     session: {
       'session length': sessionIds.size,
     },
     unacked: {
-      observed: queue.messages_unacknowledged,
       estimate: approxUnacked,
       delta: queue.messages_unacknowledged - approxUnacked,
       '% error': round2((queue.messages_unacknowledged - approxUnacked) / queue.messages_unacknowledged * 100),
@@ -122,7 +132,9 @@ class Publisher {
   async sendMessage() {
     const message = Buffer.from('read as ASAP as possible, then burn');
     this.idCounter += 1;
-    console.log(`Publishing message ${this.idCounter}`);
+    if (this.idCounter % 1000 === 0) {
+      console.log(`Publishing message ${this.idCounter}`);
+    }
     const pubBuffHasSpace = this.channel.sendToQueue(QUEUE_NAME, message, {
       correlationId: `${this.idCounter}`,
     });
@@ -160,13 +172,12 @@ class BackpressureAwareConsumer {
     // if there is still room in the buffer
     // allow as many as possible in
     // NOTE: need to handle v3.3.0 global semantics: https://www.rabbitmq.com/consumer-prefetch.html
-    this.prefetchAmount = CONSUMER_MAX_BUFFER_LENGTH;
-    this.channel.prefetch(this.prefetchAmount);
+    this.prefetchCount = CONSUMER_MAX_BUFFER_LENGTH;
+    this.channel.prefetch(this.prefetchCount, false); // not global
   }
 
   consume() {
-    this.channel.consume(QUEUE_NAME, (msg) => {
-      // TODO: incorporate variable prefetch amount
+    this.channel.consume(QUEUE_NAME, async (msg) => {
       this.buffer.push(msg);
     });
   }
@@ -230,19 +241,33 @@ async function ackSessionIds() {
   sessionMessages = [];
 }
 
+async function checkpointPollingLoop() {
+  let lastCheckpointTime = Date.now();
+  while (isRunning) {
+    await sleep(CHECKPOINT_POLL_MS);
+    const now = Date.now();
+    if (lastCheckpointTime + CHECKPOINT_INTERVAL_MS < now) {
+      console.log('checkpointing');
+      await ackSessionIds();
+      lastCheckpointTime = now;
+    }
+  }
+}
+
 async function runLoop(onErr) {
-  consumer.consume();
+  statsLoop()
+    .catch(onErr);
+
+  checkpointPollingLoop()
+    .catch(onErr);
 
   publisher
     .publish()
     .catch(onErr);
 
-  await sleep(PUBLISHER_HEAD_START_MS);
+  await sleep(PUBLISH_HEAD_START_MS);
 
-  statsLoop()
-    .catch(onErr);
-
-  let lastCheckpointTime = Date.now();
+  consumer.consume();
 
   let runNum = 0;
   while (isRunning) {
@@ -250,13 +275,9 @@ async function runLoop(onErr) {
     try {
       console.log(`running ${runNum}`);
       await run();
-      await sleep(RUN_LOOP_SLEEP_MS);
-      const now = Date.now();
-      if (lastCheckpointTime + RUN_LOOP_CHECKPOINT_INTERVAL_MS < now) {
-        console.log(`checkpointing run ${runNum}`);
-        await ackSessionIds();
-        lastCheckpointTime = now;
-      }
+      lastProcessingDuration = RUN_LOOP_SLEEP_MS + (RUN_LOOP_SINE_COEFF * Math.sin(Date.now() * RUN_LOOP_SINE_ANGLE_COEFF));
+      console.log('Processing duration', lastProcessingDuration);
+      await sleep(lastProcessingDuration);
     } catch (err) {
       onErr(err);
     }
@@ -264,10 +285,19 @@ async function runLoop(onErr) {
 }
 
 async function close() {
-  return Promise.allSettled([
+  console.log('Closing');
+  await Promise.allSettled([
+    publisher.channel.close(),
+    consumer.channel.close(),
+  ]);
+
+  console.log('Channels closed');
+
+  await Promise.allSettled([
     publisher.connection.close(),
     consumer.connection.close(),
   ]);
+  console.log('Connections closed');
 }
 
 // main entrypoint
@@ -279,21 +309,21 @@ async function close() {
       type: 'boolean',
       default: false,
     })
+    .help()
     .parse();
 
   shouldPurgeQueue = args['purge-queue'];
 
-  const onErr = (err) => {
+  const shutdown = (err) => {
     if (err) {
       console.error(err);
     }
     isRunning = false;
     publisher.isRunning = false;
   };
-  process.once('SIGINT', e => onErr(e));
+  process.once('SIGINT', e => shutdown(e));
+
   await open();
-  await runLoop(onErr);
+  await runLoop(e => shutdown(e));
   await close();
-  console.log('Connections closed');
-  await sampleStats();
 })();
